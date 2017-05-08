a9808207147ed0d671e7cec4227d7952588ee68c
Ikechi Michael <mykehell123@gmail.com>
2017-05-08 18:34:40 +0100

    added Intents.Mvc sample project

---
+++ Intents.Mvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Intents.Mvc.MvcApplication" Language="C#" %>
---
+++ Intents.Mvc/Global.asax.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using Intents;
+using Intents.Web;
+
+namespace Intents.Mvc
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            IntentManager.GetIntentManager(new SessionRepository()).Register(new Intent<string>()
+            {
+                name = "page-request",
+                trigger = "page-request",
+                action = (data) =>
+                {
+                    Console.WriteLine("New Page Request");
+                }
+            });
+        }
+
+        protected void Session_Start()
+        {
+            IntentManager.GetIntentManager().CreateSessionStorage();
+        }
+
+        protected void Application_BeginRequest()
+        {
+            IntentManager.GetIntentManager().Trigger("login");
+        }
+    }
+}
